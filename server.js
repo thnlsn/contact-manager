c50951efdd043b30b7930d53ed9db9db7fe7d98c
Thomas Nelson <thnlsn@gmail.com>
2020-01-05 16:57:08 -0800

    created basic express server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 // Entry point to the back-end
+const express = require('express');
+
+// Init express in app variable
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
