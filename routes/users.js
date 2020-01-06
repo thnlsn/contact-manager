07a2dfdc0674d1710067d20b630f9dd0630ef844
Thomas Nelson <thnlsn@gmail.com>
2020-01-06 14:13:42 -0800

    added secret to default.json for jwt

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -19,13 +21,45 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
-    const errors = validationResult(req);
+  async (req, res) => {
+    const errors = validationResult(req); // errors for if any of the above fields did not match their validations
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10); // 10 is how secure the salt is
+
+      user.password = await bcrypt.hash(password, salt); // set user password to the hash rather than the original
+
+      await user.save();
+
+      // payload is the object to send to user when logged in
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(payload);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
