7421bca8015eabe7b2e88e470fc6f1a5dc06f80c
Thomas Nelson <thnlsn@gmail.com>
2020-01-07 17:45:38 -0800

    added in dependencies for the contacts routes file

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,6 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route       GET api/contacts
 // @desc        Get all users contacts (NOT ALL CONTACTS ON THE DATABASE)
